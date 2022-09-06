84a4ceccaf29e479ccd0b56715c9bf80992a8eef
Zachary Flegle <zflegle3@gmail.com>
2022-09-05 23:43:12 -0400

    Added Login component including Firebase based google authentication

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
+
+
 import logo from './logo.svg';
 import { useState } from "react";
 import './App.css';
 import Nav from "./components/Nav";
+import Login from "./components/Login"
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD0ZLG9uo5ApNhWCZF-R3XqjwiZZSKa9Bw",
+  authDomain: "to-do-list-ab042.firebaseapp.com",
+  projectId: "to-do-list-ab042",
+  storageBucket: "to-do-list-ab042.appspot.com",
+  messagingSenderId: "401959567858",
+  appId: "1:401959567858:web:ed0a0f6b3adea686df2b6b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+// provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+// const db = getFirestore(app);
+
+
+
+
 
 function App() {
+  const [userCurrent, setUserCurrent] = useState(false);
 
 
 
 
-
-  return (
-    <div className="App">
-      <div className="content">
-        <div className="left"></div>
+  if (!userCurrent) {
+    return (
+      <Login auth={auth} provider={provider} setUserCurrent={setUserCurrent}/>
+    );
+  } else {
+    return (
+      <div className="App">
+        <div className="content">
+          <div className="left"></div>
+        </div>
+        <Nav/>
       </div>
-      <Nav/>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
